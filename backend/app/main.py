ca593fe4ae54dbba72a4fc08989dacb0c1a4b19d
Shunsuke Shimojo <>
2025-11-09 04:49:09 +0900

    Add CORS settings for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # フロントエンドのURL
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
